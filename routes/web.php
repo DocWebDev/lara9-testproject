4079e4a958ae413db3b7909af0d9b63cac454597
HaudegenHH <einsundeins79@gmx.de>
2022-10-15 13:50:30 +0200

    creating routes

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,30 @@
 <?php
 
+use Barryvdh\Debugbar\Facades\Debugbar;
+use Barryvdh\Debugbar\Twig\Extension\Debug;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostsController;
+use App\Http\Controllers\HomeController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+// return a view directly
+// Route::view('/posts', 'posts.index', ['name' => 'Fun Fun Function']);
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// route for __invoke method
+Route::get('/', HomeController::class)->name('home.index');
+
+// GET
+// Route::get('/posts/create', [PostsController::class, 'index'])->name('posts.index');
+// Route::get('/posts/{id}', [PostsController::class, 'show'])->name('posts.show');
+
+// // POST
+// Route::get('/posts', [PostsController::class, 'create'])->name('posts.create');
+// Route::post('/posts', [PostsController::class, 'store'])->name('posts.store');
+
+// // PUT or PATCH
+// Route::get('/posts/edit/{id}', [PostsController::class, 'edit'])->name('posts.edit');
+// Route::post('/posts/{id}', [PostsController::class, 'update'])->name('posts.update');;
+
+// // DELETE
+// Route::delete('/posts/{id}', [PostsController::class, 'destroy'])->name('posts.destroy');
+
+Route::resource('posts', PostsController::class);
