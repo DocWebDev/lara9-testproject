60b50ca991ae394cf1fa58127a843b5a4a8a291e
HaudegenHH <einsundeins79@gmx.de>
2022-10-15 23:22:44 +0200

    most important querybuilder methods

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -14,17 +14,57 @@ class PostsController extends Controller
      */
     public function index()
     {
+
+
         // prepared statements with query builder
         // $posts = DB::select('SELECT * FROM posts WHERE id = ?', [1]);
         // or named params
         // $posts = DB::select('SELECT * FROM posts WHERE id = :id', ['id' => 1]);
 
-        $post = DB::delete('DELETE FROM posts WHERE id = ?', [3]);
-        dd($post);
-
         // echo "<pre>";
         // print_r($posts);
         // die();
+
+        // $post = DB::delete('DELETE FROM posts WHERE id = ?', [3]);
+        // dd($post);
+
+        // chaining & selecting spec. columns
+        // $posts = DB::table('posts')
+            // ->select('title', 'body')
+            // ->where('id', '>', 20)
+            // ->where('id', 20)
+            // ->where('is_published', true)
+            // ->whereBetween('min_to_read', [2, 4])
+            // ->whereIn('min_to_read', [2, 4, 6])
+            // ->whereNull('excerpt')
+            // ->whereNotNull('created_at')
+            // ->select('min_to_read')
+            // ->distinct() // filters out rows with unique values for 'min_to_read' column
+            // ->orderBy('id', 'DESC')
+            // ->skip(5)      // useful for custom pagination (skip & take)
+            // ->take(10)      // amount of rows that you want to select (perPage)
+            // ->inRandomOrder()
+            // ->get();
+
+        // instead of ->get()  ->first()  (which sets the limit to 1 implicitly)
+        $posts = DB::table('posts')
+            // ->where('id', 5)
+            // ->first();
+            // ->find(5); // replaces the previous where & first
+            // ->where('id', 5)
+            // ->value('body'); // print out the content of post with id 5
+            // ->count();    // with param it shows the row count..or with condition:
+            // ->where('id', '>', 5)
+            // ->count();
+            // ->min('min_to_read');  // just the minimum for that column
+            // ->max('min_to_read');
+            // ->sum('min_to_read');
+            ->avg('min_to_read');
+
+
+        dd($posts);
+
+
 
         return view('posts.index');
     }
