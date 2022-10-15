821afd48963c65799aade6620142eeccbed65cea
HaudegenHH <einsundeins79@gmx.de>
2022-10-15 15:04:09 +0200

    route prefixing

--- routes/web.php
+++ routes/web.php
@@ -9,22 +9,18 @@ use App\Http\Controllers\HomeController;
 // return a view directly
 // Route::view('/posts', 'posts.index', ['name' => 'Fun Fun Function']);
 
+// Route::resource('posts', PostsController::class);
+
+// prefixing route
+Route::prefix('/posts')->group(function() {
+    Route::get('/', [PostsController::class, 'index'])->name('posts.index');
+    Route::get('/{id}', [PostsController::class, 'show'])->name('posts.show');
+    Route::get('/create', [PostsController::class, 'create'])->name('posts.create');
+    Route::post('/', [PostsController::class, 'store'])->name('posts.store');
+    Route::get('/edit/{id}', [PostsController::class, 'edit'])->name('posts.edit');
+    Route::post('/{id}', [PostsController::class, 'update'])->name('posts.update');;
+    Route::delete('/{id}', [PostsController::class, 'destroy'])->name('posts.destroy');
+});
+
 // route for __invoke method
 Route::get('/', HomeController::class)->name('home.index');
-
-// GET
-// Route::get('/posts/create', [PostsController::class, 'index'])->name('posts.index');
-// Route::get('/posts/{id}', [PostsController::class, 'show'])->name('posts.show');
-
-// // POST
-// Route::get('/posts', [PostsController::class, 'create'])->name('posts.create');
-// Route::post('/posts', [PostsController::class, 'store'])->name('posts.store');
-
-// // PUT or PATCH
-// Route::get('/posts/edit/{id}', [PostsController::class, 'edit'])->name('posts.edit');
-// Route::post('/posts/{id}', [PostsController::class, 'update'])->name('posts.update');;
-
-// // DELETE
-// Route::delete('/posts/{id}', [PostsController::class, 'destroy'])->name('posts.destroy');
-
-Route::resource('posts', PostsController::class);
